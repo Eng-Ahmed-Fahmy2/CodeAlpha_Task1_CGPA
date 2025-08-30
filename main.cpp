f8217e8b3a1b56c22031f19aaa54231acf91a3ae
Eng-Ahmed-Fahmy2 <ahmedfahmymohammed214@gmail.com>
2025-08-30 22:48:51 +0300

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -6,10 +6,7 @@ using namespace std;
 int main() {
     int n;
     cout << "Enter number of courses: ";
-    if(!(cin >> n) || n <= 0){
-        cerr << "Invalid number of courses.\n";
-        return 1;
-    }
+    cin >> n;
 
     vector<double> grade(n), credit(n);
     double totalCredits = 0, totalPoints = 0;
@@ -20,18 +17,8 @@ int main() {
         cout << "Enter credit hours for course " << i+1 << ": ";
         cin >> credit[i];
 
-        if(grade[i] < 0 || grade[i] > 4 || credit[i] <= 0){
-            cerr << "Invalid input at course " << i+1 << ".\n";
-            return 1;
-        }
-
         totalCredits += credit[i];
-        totalPoints  += grade[i] * credit[i];
-    }
-
-    if(totalCredits == 0){
-        cerr << "Total credits cannot be zero.\n";
-        return 1;
+        totalPoints += grade[i] * credit[i];
     }
 
     double CGPA = totalPoints / totalCredits;
@@ -40,3 +27,4 @@ int main() {
     cout << "\nYour CGPA is: " << CGPA << endl;
     return 0;
 }
+
